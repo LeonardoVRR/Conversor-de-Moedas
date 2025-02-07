a40dab3a1e0a81c6bebd81ddcfbdf72d93e222f9
Leonardo Vitor <leonardovitor.rego@gmail.com>
2025-02-07 09:09:45 -0300

    ajustes no layout p/ melhoria da responsividae e melhoria na lógica de conversão de moedas

--- main.js
+++ main.js
@@ -19,32 +19,53 @@ const valor_moedas = {
     real: {
         moeda: "Real",
         img: "imgs/brasil icon.png",
-        valor: 0.00671
+        valor: 0.00671,
+        formatacao: new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
     },
     dolar: {
         moeda: "Dólar americano",
         img: "imgs/estados-unidos icon.png",
-        valor: 5.8
+        valor: 5.8,
+        formatacao: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
     },
     euro: {
         moeda: "Euro",
         img: "imgs/euro icon.png",
-        valor: 6.03
+        valor: 6.03,
+        formatacao: new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        })
     },
     iene: {
         moeda: "Iene",
         img: "imgs/iene icon.png",
-        valor: 0.038
+        valor: 0.038,
+        formatacao: new Intl.NumberFormat('ja-JP', {
+            style: 'currency',
+            currency: 'JPY'
+        })
     },
     libra: {
         moeda: "Libra",
         img: "imgs/libra icon.png",
-        valor: 7.26
+        valor: 7.26,
+        formatacao: new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: 'GBP'
+        })
     },
     bitcoin: {
         moeda: "Bitcoin",
         img: "imgs/bitcoin icon.png",
-        valor: 559672.62
+        valor: 559672.62,
+        formatacao: new Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 8 })
     }
 }
 
@@ -91,6 +112,10 @@ function changeIconCoin(coin, coin_symbol) {
             break;
     }
 }
+
+value_to_convert.addEventListener("input", () => {
+    value_to_convert.value = value_to_convert.value.replace(/[^0-9.,]/g, '');
+})
         
 conversor_form.addEventListener("submit", (event)=>{
     event.preventDefault()
@@ -101,14 +126,35 @@ conversor_form.addEventListener("submit", (event)=>{
 })
 
 function calculateConversion() {
-    value_coin_convert.textContent = `${value_to_convert.value}`
+    value_coin_convert.textContent = `${(valor_moedas[coin_convert].formatacao).format(Number(value_to_convert.value).toFixed(2))}`
     country_flag_convert.setAttribute("src", `${valor_moedas[coin_convert].img}`)
     coin_name_convert.textContent = `${valor_moedas[coin_convert].moeda}`
 
     country_flag_target.setAttribute("src", `${valor_moedas[coin_target].img}`)
     coin_name_target.textContent = `${valor_moedas[coin_target].moeda}`
 
-    const result = (value_to_convert.value / valor_moedas[coin_target].valor).toFixed(2)
+    const price_coin_convert = valor_moedas[coin_convert].valor
+    const price_coin_target = valor_moedas[coin_target].valor
 
-    value_coin_target.textContent = `${result}`
+    let result = null
+
+    if (price_coin_convert > price_coin_target) {
+        result = value_to_convert.value * price_coin_convert
+    }
+
+    else if (price_coin_convert === price_coin_target) {
+        result = value_to_convert.value * 1
+    }
+
+    else {
+        result = value_to_convert.value / price_coin_target
+    }
+
+    if (coin_target === "bitcoin") {
+        value_coin_target.textContent = `${(valor_moedas[coin_target].formatacao).format(result)} BTC`
+    }
+
+    else {
+        value_coin_target.textContent = `${(valor_moedas[coin_target].formatacao).format(result.toFixed(2))}`
+    }
 }
